31f96d92cf3234aa9c317ad54b9f98966fdbb56c
Sandeep2027 <166156895+Sandeep2027@users.noreply.github.com>
2025-09-28 23:05:40 +0530

    Delete backend directory

--- backend/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const userController = require('../controllers/userController');
-
-const router = express.Router();
-
-router.get('/profile', userController.getProfile);
-router.get('/', userController.getUsers);
-
-module.exports = router;
